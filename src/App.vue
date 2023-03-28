01c155d19e558f0fbe483693d7d96f141ddf5008
abdulazizv <abdulazizheir@gmail.com>
2023-03-28 14:39:43 +0500

    message

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <Header />
+  </div>
+</template>
+<script>
+import Header from './components/Header/Header.vue';
+export default {
+  name: "App",
+  components:{Header}
+};
+</script>
+<style lang="css"></style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+        <header class="sticky bg-[#FFFFFF]">
+        <div class="wrapper shadow-2xl h-[70px] sticky flex flex-row">
+                <div class="logo ml-[200px] flex items-center">
+                    <div class="wrapper__logo bg-[#A53DFF] flex items-center justify-center rounded-3xl px-2 py-3">
+                        <h1 class="text-white px-3 font-bold text-xl">A</h1>
+                    </div> <h1 class="py-3 ml-1 text-[#132238] text-3xl font-bold">Abdulaziz</h1>
+                </div>
+                <ul class="flex flex-row items-center justify-around ml-[160px] max-w-[652px] w-full text-lg font-semibold text-[#333333]">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Process</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                    <li>Services</li>
+                    <button class="btn bg-[#A53DFF] text-lg font-bold px-3 py-2 text-white rounded-lg hover:ring-2 hover:outline-none">Contact</button>
+                </ul>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    name:'Header',
+}
+</script>
+<style>
+    
+</style>

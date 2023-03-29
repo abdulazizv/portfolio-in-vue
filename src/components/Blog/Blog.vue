82624247ed61a4e6ea2d30999fa9460c59ba2490
abdulazizv <abdulazizheir@gmail.com>
2023-03-30 00:53:47 +0500

    addedd

--- src/components/Blog/Blog.vue
+++ src/components/Blog/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto wrapper">
+  <div class="container mx-auto wrapper flex gap-4 flex-col">
   <div class="flex flex-col gap-2">
     <h1 class="text-center font-bold text-5xl text-[#132238]">Blog</h1>
     <div class="header_p justify-center flex">
@@ -9,6 +9,28 @@
         There are many variations of passages of Lorem Ipsum available, but the
         majority have suffered alteration.
       </p>
+    </div>
+  </div>
+  <div class="articles flex flex-row gap-5">
+    <div class="about__post w-[315px] bg-white shadow-2xl">
+        <img src="../../assets/unsplash_phIFdC6lA4E.png" alt="unsplash1">
+        <h1 class="date__op px-3 py-2">22 oct,2020 / 246 comments</h1>
+        <p class="text-[#333333] text-18px font-semibold px-3 py-2">Lorem ipsum dolor sit consea. Nulla purus arcu</p>
+    </div>
+    <div class="about__post w-[315px] bg-white shadow-2xl">
+        <img src="../../assets/yellowblackunsplash.png" alt="unsplash1">
+        <h1 class="date__op">22 oct,2020 / 246 comments</h1>
+        <p class="">Lorem ipsum dolor sit consea. Nulla purus arcu</p>
+    </div>
+    <div class="about__post w-[315px] bg-white shadow-2xl">
+        <img src="../../assets/rgbunsplash.png" alt="unsplash1">
+        <h1 class="date__op">22 oct,2020 / 246 comments</h1>
+        <p class="">Lorem ipsum dolor sit consea. Nulla purus arcu</p>
+    </div>
+    <div class="about__post w-[315px] bg-white shadow-2xl">
+        <img src="../../assets/blueskyunsplash.png" alt="unsplash1">
+        <h1 class="date__op">22 oct,2020 / 246 comments</h1>
+        <p class="">Lorem ipsum dolor sit consea. Nulla purus arcu</p>
     </div>
   </div>
   </div>
--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="footer flex justify-center h-[70px] items-center bg-cyan-500">
+    <div class="footer flex justify-center h-[70px] items-center bg-cyan-500 mt-10">
         <h1 class="text-xl font-bold">This is footer</h1>
     </div>
 </template>
